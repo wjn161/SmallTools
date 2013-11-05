0598a97dd64096594d6765629c34752db9527143
wjn161 <wjn-161@163.com>
2013-11-05 14:02:06 +0800

    调整名字空间引用，添加注释

--- DateCalculator/DateCalculator/MainWindow.xaml.cs
+++ DateCalculator/DateCalculator/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DateCalculator
 {
@@ -27,7 +15,9 @@ namespace DateCalculator
             txtEnd.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             Calculate();
         }
-
+        /// <summary>
+        /// 计算逻辑
+        /// </summary>
         private void Calculate()
         {
             try
