713771d6f8de43e7dbb7344ddc451eff1a1778eb
wjn161 <wjn-161@163.com>
2013-11-07 22:55:41 +0800

    add setting page front code
    add setting page front code with jquery mobile

---
+++ WhereToEat/WhereToEat.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WhereToEat.Web
+{
+    public class BundleConfig
+    {
+        // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/vendor/jquery/jquery-1.10.2.min.js",
+                        "~/Scripts/vendor/jquerymobile/jquery.mobile-1.3.2.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/index").Include(
+                "~/Scripts/src/index.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Scripts/vendor/jquerymobile/jquery.mobile-1.3.2.min.css",
+                      "~/Content/css/site.css"));
+        }
+    }
+}
---
+++ WhereToEat/WhereToEat.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WhereToEat.Web.MvcApplication" Language="C#" %>
